bc7d05b44f792d897ddc5206a33ec3f5b31dede5
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-03-07 09:00:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Gurmail Singh");
+$("#policyNumber").text("SC92072820");
+$("#startDate").text("08-Feb-2020");
+$("#endDate").text("07-Feb-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DD1375");
+$("#make").text("TVS MOTORS");
+$("#model").text("JUPITER");
+$("#vehicalSubType").text("DISC BRAKE");
+$("#engineNo").text("BG4KF2067966");
+$("#chassis").text("MD626BG11F3K60351");
